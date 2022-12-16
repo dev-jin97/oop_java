087212aef9c8db4fa0087d697e3b77a1f61b3c67
Hyeonjin Kim <ilhj1228@gmail.com>
2022-12-16 18:30:31 +0900

    커밋할 변경 사항: study to generics 	새 파일:       oop_prj_de/src/com/dev_jin97/de/java/c05_generics/Main.java

---
+++ oop_prj_de/src/com/dev_jin97/de/java/c05_generics/Main.java
@@ -0,0 +1,15 @@
+package com.dev_jin97.de.java.c05_generics;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList();
+
+        Collection<String> collection = list;
+
+
+    }
+}
